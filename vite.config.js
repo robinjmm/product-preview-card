95d7aeb2cbde209542d4b489f4c7af0b4fe6ef22
J.M. Robin <johnmichaelrobin@gmail.com>
2023-01-01 15:03:19 +0800

    Add autoprefixer and move PostCSS config to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import {defineConfig} from "vite";
 import {createHtmlPlugin} from "vite-plugin-html";
+import autoprefixer from "autoprefixer";
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
 
 export default defineConfig({
 	plugins: [
         createHtmlPlugin({
 			minify: true
         })
-    ]
+	],
+	css: {
+        postcss: {
+            plugins: [
+                autoprefixer,
+				purgeCSSPlugin({
+					content: ["index.html"],
+					variables: true,
+					keyframes: true
+                })
+            ]
+        }
+    }
 })
